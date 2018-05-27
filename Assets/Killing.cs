84d0c615563f3c366da82a24f3552af98d4101dc
MiloszRatajczyk <Milosz2002ratajczyk@gmail.com>
2018-05-27 09:45:14 +0200

    First commit

---
+++ Assets/Killing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class Killing : MonoBehaviour
+    {
+
+        private PlatformerCharacter2D m_Character;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //if (m_Character.GetComponent(""))
+            //{
+            //    m_Character.Destroy();
+            //}
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BtnNew ()
+    {
+        Debug.Log("New");
+        SceneManager.LoadScene("NewGameScene");
+    }
+
+    public void BtnLoad()
+    {
+        Debug.Log("Load");
+        SceneManager.LoadScene("LoadGameScene");
+    }
+
+    public void BtnOptions()
+    {
+        Debug.Log("Options");
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+}
